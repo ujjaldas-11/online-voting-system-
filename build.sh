6c2498b14bd4fa7c9b6d55688729a4134a440581
ujjaldas-11 <dasu82058@gmail.com>
2026-04-19 14:18:44 +0530

    created build file

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files (important for production)
+python manage.py collectstatic --no-input
+
+# Run migrations (this creates all tables, including voting_vote)
+python manage.py migrate --no-input
