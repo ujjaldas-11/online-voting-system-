62cbdaecc39e8cedcc704002373fc8c55ead580b
ujjaldas-11 <dasu82058@gmail.com>
2026-04-19 23:21:34 +0530

    made changes in build file

--- build.sh
+++ build.sh
@@ -11,5 +11,3 @@ python manage.py collectstatic --no-input
 # Run migrations (this creates all tables, including voting_vote)
 python manage.py migrate --no-input
 
-
-python manage.py createsuperuser --noinput || true
